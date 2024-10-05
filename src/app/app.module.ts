d334b4343dd26553fb0b3967adb1563dfabb6a06
Poonam-gaian <poonam.r@gaiansolutions.com>
2024-10-05 18:58:07 +0530

    update the angular from 10 to 11 and other respected libraries also

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,65 +3,68 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './@core/core.module';
-import { ThemeModule } from './@theme/theme.module';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { CoreModule } from "./@core/core.module";
+import { ThemeModule } from "./@theme/theme.module";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
 import {
-  NbChatModule,
-  NbDatepickerModule,
-  NbDialogModule,
-  NbMenuModule,
-  NbSidebarModule,
-  NbToastrModule,
-  NbCardModule,
-  NbWindowModule,
-  NbLayoutModule,
-} from '@nebular/theme';
-import { DeleteDialogComponent } from './pages/dialog/delete-dialog/delete-dialog.component';
-import { DialogModule } from './pages/dialog/dialog.module';
-import { LoginComponent } from './pages/login/login.component';
-import { ForgotComponent } from './pages/forgot/forgot.component';
-import { FormsModule } from '@angular/forms';
-import { ChangepasswordComponent } from './pages/changepassword/changepassword.component'
-import { HttpModule } from '@angular/http';
-import { AdminProfileComponent } from './pages/admin-profile/admin-profile.component';
+	NbChatModule,
+	NbDatepickerModule,
+	NbDialogModule,
+	NbMenuModule,
+	NbSidebarModule,
+	NbToastrModule,
+	NbCardModule,
+	NbWindowModule,
+	NbLayoutModule,
+} from "@nebular/theme";
+import { DeleteDialogComponent } from "./pages/dialog/delete-dialog/delete-dialog.component";
+import { DialogModule } from "./pages/dialog/dialog.module";
+import { LoginComponent } from "./pages/login/login.component";
+import { ForgotComponent } from "./pages/forgot/forgot.component";
+import { FormsModule } from "@angular/forms";
+import { ChangepasswordComponent } from "./pages/changepassword/changepassword.component";
+import { AdminProfileComponent } from "./pages/admin-profile/admin-profile.component";
 // import { HouseownerdetailsModule } from './pages/houseowner/houseownerdetails/houseownerdetails.module';
-import { PasswordStrengthBarModule } from './pages/password-strength-bar/password-strength-bar.module';
-import { ConfirmpasswordComponent } from './pages/login/confirmpassword/confirmpassword.component';
-
+import { PasswordStrengthBarModule } from "./pages/password-strength-bar/password-strength-bar.module";
+import { ConfirmpasswordComponent } from "./pages/login/confirmpassword/confirmpassword.component";
 
 @NgModule({
-  declarations: [AppComponent, ForgotComponent, ChangepasswordComponent, AdminProfileComponent, LoginComponent, ConfirmpasswordComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    ThemeModule.forRoot(),
-    HttpModule,
-    NbSidebarModule.forRoot(),
-    NbMenuModule.forRoot(),
-    NbDatepickerModule.forRoot(),
-    NbDialogModule.forRoot(),
-    NbWindowModule.forRoot(),
-    NbToastrModule.forRoot(),
-    NbCardModule,
-    NbLayoutModule,
-    NbChatModule.forRoot({
-      messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
-    }),
-    CoreModule.forRoot(),
-    DialogModule,
-    FormsModule,
-    PasswordStrengthBarModule
-  ],
-  bootstrap: [AppComponent],
-  entryComponents: [ForgotComponent, AdminProfileComponent],
+	declarations: [
+		AppComponent,
+		ForgotComponent,
+		ChangepasswordComponent,
+		AdminProfileComponent,
+		LoginComponent,
+		ConfirmpasswordComponent,
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		AppRoutingModule,
+		ThemeModule.forRoot(),
+		NbSidebarModule.forRoot(),
+		NbMenuModule.forRoot(),
+		NbDatepickerModule.forRoot(),
+		NbDialogModule.forRoot(),
+		NbWindowModule.forRoot(),
+		NbToastrModule.forRoot(),
+		NbCardModule,
+		NbLayoutModule,
+		NbChatModule.forRoot({
+			messageGoogleMapKey: "AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY",
+		}),
+		CoreModule.forRoot(),
+		DialogModule,
+		FormsModule,
+		PasswordStrengthBarModule,
+	],
+	bootstrap: [AppComponent],
+	entryComponents: [ForgotComponent, AdminProfileComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/service/webapiservice.service.ts
+++ src/app/service/webapiservice.service.ts
@@ -1,85 +1,82 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { Http, RequestOptions, Headers } from '@angular/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Http, RequestOptions, Headers } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: "root",
 })
 export class WebapiserviceService {
-  baseUrl = environment.url.baseUrl;
-  constructor(
-    private http: Http,
-    ) {
-  }
+	baseUrl = environment.url.baseUrl;
+	constructor(private http: Http) {}
 
-  get(url) {
-    const headers = this.setHeader();
-    const options = new RequestOptions({
-      headers: headers,
-    });
+	get(url) {
+		const headers = this.setHeader();
+		const options = new RequestOptions({
+			headers: headers,
+		});
 
-    return this.http.get(this.baseUrl + url, options);
-  }
+		return this.http.get(this.baseUrl + url, options);
+	}
 
-  post(url, json){
-    const headers = this.setHeader();
-    const options = new RequestOptions({
-      headers: headers,
-    });
-    return this.http.post(this.baseUrl + url, json, options);
-  }
+	post(url, json) {
+		const headers = this.setHeader();
+		const options = new RequestOptions({
+			headers: headers,
+		});
+		return this.http.post(this.baseUrl + url, json, options);
+	}
 
-  fordataPost(url, json){
-    const headers = this.setFormHeader();
-    const options = new RequestOptions({
-      headers: headers,
-    });
-    return this.http.post(this.baseUrl + url, json, options);
-  }
+	fordataPost(url, json) {
+		const headers = this.setFormHeader();
+		const options = new RequestOptions({
+			headers: headers,
+		});
+		return this.http.post(this.baseUrl + url, json, options);
+	}
 
-  setHeader() {
-    const headers = new Headers({
-      'Content-Type': 'application/json',
-    });
+	setHeader() {
+		const headers = new Headers({
+			"Content-Type": "application/json",
+		});
 
-    if (localStorage.getItem('admin_details') !== null) {
-      const getToken = localStorage.getItem('token');
-      // getToken = getToken.replace(/"/g, '');
+		if (localStorage.getItem("admin_details") !== null) {
+			const getToken = localStorage.getItem("token");
+			// getToken = getToken.replace(/"/g, '');
 
-      headers.append('x-access-token', getToken);
-    } else if (localStorage.getItem('admin_forgot') !== null) {
-      const getToken = localStorage.getItem('admin_forgot');
-      // getToken = getToken.replace(/"/g, '');
+			headers.append("x-access-token", getToken);
+		} else if (localStorage.getItem("admin_forgot") !== null) {
+			const getToken = localStorage.getItem("admin_forgot");
+			// getToken = getToken.replace(/"/g, '');
 
-      headers.append('x-access-token', getToken);
-    } else if (localStorage.getItem('confirm_token') !== null) {
-      let getToken = localStorage.getItem('confirm_token');
-      // getToken = getToken.replace(/"/g, '');
+			headers.append("x-access-token", getToken);
+		} else if (localStorage.getItem("confirm_token") !== null) {
+			let getToken = localStorage.getItem("confirm_token");
+			// getToken = getToken.replace(/"/g, '');
 
-      headers.append('x-access-token', getToken);
-    }
+			headers.append("x-access-token", getToken);
+		}
 
-    return headers;
-  }
+		return headers;
+	}
 
-  setFormHeader() {
-    const headers = new Headers();
+	setFormHeader() {
+		const headers = new Headers();
 
-    if (localStorage.getItem('admin_details') !== null) {
-      const getToken = localStorage.getItem('token');
-      // getToken = getToken.replace(/"/g, '');
+		if (localStorage.getItem("admin_details") !== null) {
+			const getToken = localStorage.getItem("token");
+			// getToken = getToken.replace(/"/g, '');
 
-      headers.append('x-access-token', getToken);
-    }
+			headers.append("x-access-token", getToken);
+		}
 
-    if (localStorage.getItem('admin_forgot') !== null) {
-      const getToken = localStorage.getItem('admin_forgot');
-      // getToken = getToken.replace(/"/g, '');
+		if (localStorage.getItem("admin_forgot") !== null) {
+			const getToken = localStorage.getItem("admin_forgot");
+			// getToken = getToken.replace(/"/g, '');
 
-      headers.append('x-access-token', getToken);
-    }
+			headers.append("x-access-token", getToken);
+		}
 
-    return headers;
-  }
+		return headers;
+	}
 }
